ba8b4077d338e8c8ce415d2af804f2c1bbf215da
Paul Stansifer <paul.stansifer@gmail.com>
2011-02-21 17:54:07 -0500

    Pulled user-lookup out of page rendering.  (still need to handle errors in it, of course)

--- webside.hs
+++ webside.hs
@@ -1,26 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Database.HDBC
 import Database.HDBC.SqlValue
-import Database.HDBC.PostgreSQL (connectPostgreSQL)
-import Happstack.Server (nullConf, simpleHTTP, ok, dir, FromReqURI(..), path)
-
+import Database.HDBC.PostgreSQL (connectPostgreSQL, Connection)
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, FromReqURI(..), path, ServerPart, Response, toResponse)
+import Happstack.Server.SimpleHTTP (ServerPartT)
 
 import Control.Monad
 import Control.Monad.Trans (liftIO)
 
 
-data Username = Username String
+
+import           Text.Blaze ((!), toHtml)
+import qualified Text.Blaze.Html4.Strict as H
+import qualified Text.Blaze.Html4.Strict.Attributes as A
+
+
+--from the BlazeHtml tutorial
+appTemplate :: String  -> H.Html -> H.Html
+appTemplate title body =
+  H.html $ do
+    H.head $ do
+      H.title (H.string $ title ++ " - Reminded by Vegans")
+      H.meta ! A.httpEquiv "Content-Type" ! A.content "text/html;charset=utf-8"
+    H.body $ do
+      body
+
+newtype Username = Username String
 
 
 instance FromReqURI Username where
   fromReqURI name = Just $ Username name
 
-userPage pageName fn = dir "user" $ path $ \name -> dir pageName $ (fn (Username name))
 
+userPage :: String -> (Int -> Connection -> ServerPartT IO Response) -> ServerPartT IO Response
+userPage pageName fn = dir "user" $ path $ \name -> dir pageName $ do
+  (c, uid) <- liftIO $ do
+    c <- connectPostgreSQL "host=localhost dbname=rbv user=vegans password=local_login_password"
+    v <- quickQuery' c "SELECT uid FROM usr WHERE email=?" [toSql (name :: String)]
+    print $ show v
+    [[uid]] <- quickQuery' c "SELECT uid FROM usr WHERE email=?" [toSql (name :: String)]
+    
+    return (c, fromSql uid)
+  
+  fn uid $ c
 
 main :: IO ()
-main = do
+main =
   simpleHTTP nullConf $ msum
     [ mzero,
       userPage "reminders" remindersPage -- \(Username name) -> ok $ "Hi, " ++ name
@@ -31,13 +59,15 @@ main = do
 
 
 
-remindersPage (Username name)  = do
+
+remindersPage :: Int -> Connection -> ServerPartT IO Response
+remindersPage uid c = do
   val <- liftIO $ do
-    c <- connectPostgreSQL "host=localhost dbname=rbv user=vegans password=local_login_password"
     
     print "hi!"
-    print "hihi!"
-    v <- quickQuery' c "SELECT * FROM usr" []
-    return $ fromSql $ head $ head $ v
+    v <- quickQuery' c "SELECT * FROM reminder WHERE uid=?" [toSql uid]
+    return $ toResponse $ appTemplate "Reminders" $ H.p $ toHtml $ show v
 
-  ok $ "Hi, " ++ name ++ "<br>" ++ val
+
+
+  ok val 
